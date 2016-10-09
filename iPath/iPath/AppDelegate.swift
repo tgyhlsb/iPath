2839ef9aae5cb83fe0b003b5fe6722b67e30d72f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2016-10-09 22:49:51 +0200

    clean boilerplate

--- iPath/iPath/AppDelegate.swift
+++ iPath/iPath/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        self.window = UIWindow()
+        self.window?.rootViewController = RootViewController()
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ iPath/iPath/Controllers/RootViewController/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  iPath
+//
+//  Created by Tanguy Hélesbeux on 09/10/2016.
+//  Copyright © 2016 Tanguy Helesbeux. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
