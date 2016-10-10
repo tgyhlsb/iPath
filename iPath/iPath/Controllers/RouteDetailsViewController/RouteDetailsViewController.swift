27080e17119d007487ea8a43493696b5a9c4c848
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2016-10-10 23:21:29 +0200

    draw path

--- iPath/iPath/Controllers/RouteDetailsViewController/RouteDetailsViewController.swift
+++ iPath/iPath/Controllers/RouteDetailsViewController/RouteDetailsViewController.swift
@@ -35,12 +35,64 @@ class RouteDetailsViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = self.title(from: self.route)
+        self.mapView.delegate = self
+        self.initialize(for: self.route)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.zoom(to: self.route, animated: animated)
+    }
+    
+    private func initialize(for route: Route) {
+        self.title = self.title(from: route)
+        self.mapView.add(self.overlay(from: route.places))
     }
     
     // MARK: Helpers
     
     private func title(from route: Route) -> String {
         return "\(route.places.count) places"
+    }
+    
+    // MARK: - MapView
+    
+    private func zoom(to route: Route, animated: Bool) {
+        guard let region = self.region(from: route) else { return }
+        self.mapView.setRegion(region, animated: animated)
+    }
+    
+    private func overlay(from places: [Route.Place]) -> MKPolyline {
+        let coordinates = places.map { place in
+            return CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)
+        }
+        return MKPolyline(coordinates: coordinates, count: coordinates.count)
+    }
+    
+    private func region(from route: Route) -> MKCoordinateRegion? {
+        guard let first = route.places.first else { return nil }
+        var maxLat = first.latitude
+        var maxLong = first.longitude
+        var minLat = maxLat
+        var minLong = maxLong
+        for place in route.places {
+            maxLat = max(maxLat, place.latitude)
+            maxLong = max(maxLong, place.longitude)
+            minLat = min(minLat, place.latitude)
+            minLong = min(minLong, place.longitude)
+        }
+        let span = MKCoordinateSpan(latitudeDelta: maxLat - minLat, longitudeDelta: maxLong - minLong)
+        let center = CLLocationCoordinate2D(latitude: minLat + span.latitudeDelta, longitude: minLong + span.longitudeDelta)
+        return MKCoordinateRegion(center: center, span: span)
+    }
+    
+    // MARK: MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.lineWidth = 3
+        renderer.strokeColor = UIColor.red
+        renderer.lineCap = .round
+        return renderer
     }
 }
